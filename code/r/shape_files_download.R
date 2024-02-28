601dc494b8887ea7913ce8ee2eef0d44dfa0e191
Daniel Sánchez Pazmiño <dsanchezp998@gmail.com>
2024-02-28 00:36:34 -0800

    download shapefiles and metadata

---
+++ code/r/shape_files_download.R
@@ -0,0 +1,40 @@
+# R Script: Ecuador Map Shapefiles Download
+# Laboratorio de Investigación para el Desarrollo del Ecuador 
+
+# This script downloads the map shapefiles for Ecuador.
+
+# Preliminaries ----------------------------------------------------------------
+
+# Downloading the files ---------------------------------------------------------
+
+# Declare download URLs
+
+shapefile_url <- "https://www.ecuadorencifras.gob.ec//documentos/web-inec/Cartografia/Clasificador_Geografico/2012/SHP.zip"
+
+metadata_url <- "https://www.ecuadorencifras.gob.ec//documentos/web-inec/Cartografia/Clasificador_Geografico/2012/LISTADOS.zip"
+
+# Download the zip file containing the shapefiles
+
+download.file(shapefile_url, "data/shp/ecuador_shapefiles.zip")
+
+# Download the zip file containing metadata and DPA (División Política Administrativa) metadata
+
+download.file(metadata_url, "data/shp/ecuador_dpa_metadata.zip")
+
+# Unzip the files --------------------------------------------------------------
+
+# Unzip the shapefiles
+
+unzip("data/shp/ecuador_shapefiles.zip", exdir = "data/shp/ecuador_shapefiles")
+
+# Unzip the metadata
+
+unzip("data/shp/ecuador_dpa_metadata.zip", exdir = "data/shp/ecuador_dpa_metadata")
+
+# Delete unnecessary files ------------------------------------------------------
+
+
+file.remove("data/shp/ecuador_shapefiles.zip")
+
+file.remove("data/shp/ecuador_dpa_metadata.zip")
+

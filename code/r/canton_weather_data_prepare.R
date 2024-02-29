56d3b1f8963885ca8700f0bfd959e32dc35c88ff
Daniel Sánchez Pazmiño <dsanchezp998@gmail.com>
2024-02-29 00:11:57 -0800

    fix extract weather function

--- code/r/canton_weather_data_prepare.R
+++ code/r/canton_weather_data_prepare.R
@@ -12,6 +12,9 @@ if (!require(sf)) install.packages("sf")
 if (!require(terra)) install.packages("terra")
 if (!require(haven)) install.packages("haven")
 if (!require(janitor)) install.packages("janitor")
+if(!require(stringr)) install.packages("stringr")
+if(!require(tidyr)) install.packages("tidyr")
+if(!require(lubridate)) install.packages("lubridate")
 
 # Load libraries
 
@@ -20,6 +23,8 @@ library(sf)
 library(terra)
 library(janitor)
 library(haven)
+library(stringr)
+library(tidyr)
 
 # Loading shapefile -----------------------------------------------------------
 
@@ -41,13 +46,12 @@ canton_ids <-
 # Create a function to extract daily weather data for any given NetCDF file
 # The argument is the path to the NetCDF file
 
-extract_min_temp_data <- function(x){
+extract_weather_data <- function(x){
     # Load the NetCDF file and reproject the SpatRaster to standard coordinates between -180 and 180 degrees
     x <- rast(x) %>% rotate()
     
     # Reproject the canton shapefile to the same coordinate system as the NetCDF file
-    canton_ids <- 
-        st_transform(canton_ids, crs(x))
+    canton_ids <- st_transform(canton_ids, crs(x))
     
     # Mask the SpatRaster using the shapefile
     x <- crop(x, canton_ids, mask = T)
@@ -57,15 +61,35 @@ extract_min_temp_data <- function(x){
     names <- names(x)
 
     # Extract the weighted mean temperature for each canton
-    x <- extract(x, canton_ids, fun = mean, na.rm = T, weights = T, bind = T)
+    x <- terra::extract(x, canton_ids, fun = mean, na.rm = T, weights = T, bind = T)
 
     # Transform the SpatRaster to a tibble
 
     x <- as_tibble(x)
-  
+
+    # Rename the columns to show the dates
+
+    names(x)[names(x) %in% names] <- as.character(as.Date(time))
+
+    # Pivot the tibble to long format and add a new column for the year
+
+    x <- 
+        pivot_longer(x, cols = -canton_id, names_to = "date", values_to = "value", names_transform = list(date = as.Date))  %>% 
+        mutate(year = year(date))
+
     return(x)
 }
 
 # Applying the function to the NetCDF files ------------------------------------
 
-# 
+# Getting the path to the the nc files for the minimum temperature, maximum temperature, and precipitation
+
+min_temperature_files <- list.files("data/weather/raw/tempmin",full.names = T)
+max_temperature_files <- list.files("data/weather/raw/tempmax",full.names = T)
+precipitation_files <- list.files("data/weather/raw/precip",full.names = T)
+
+# List apply the function to the NetCDF files for the minimum temperature, maximum temperature, and precipitation
+
+min_temperature_data <- lapply(min_temperature_files, extract_weather_data)
+
+min_temperature_df <- bind_rows(min_temperature_data)
